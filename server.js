70b232d4a2b563eb4b84c5d319798dd12db108dc
Derine Banis <d.banis.13@gmail.com>
2022-10-26 16:57:55 -0700

    Working on how to find levels by certain serach parameters

--- server.js
+++ server.js
@@ -20,33 +20,33 @@ const {
 } = require('graphql');
 
 //Returns an Array of Song Objects
-const getSongs = async() => {
+const getSongs = async () => {
   let songs;
-  const client = new pg.Client(dbConnection)
+  const client = new pg.Client(dbConnection);
   await client.connect()
-  .catch((err)=>console.log('Error connecting to DB', err))
+    .catch((err) => console.log('Error connecting to DB', err));
 
-  const query = 'Select * from songs'
+  const query = 'Select * from songs';
   await client.query(query)
-  .then((res)=>{songs = res.rows})
-  .then(()=>client.end())
-  .catch((err)=>console.log('Error with query', err))
-  return songs
-}
+    .then((res) => { songs = res.rows; })
+    .then(() => client.end())
+    .catch((err) => console.log('Error with query', err));
+  return songs;
+};
 //Returns an Array of Level Objects
-const getLevels = async() => {
+const getLevels = async () => {
   let levels;
-  const client = new pg.Client(dbConnection)
+  const client = new pg.Client(dbConnection);
   await client.connect()
-  .catch((err)=>console.log('Error connecting to DB', err))
+    .catch((err) => console.log('Error connecting to DB', err));
 
-  const query = 'Select * from levels'
+  const query = 'Select * from levels';
   await client.query(query)
-  .then((res)=>{levels = res.rows})
-  .then(()=>client.end())
-  .catch((err)=>console.log('Error with query', err))
-  return levels
-}
+    .then((res) => { levels = res.rows; })
+    .then(() => client.end())
+    .catch((err) => console.log('Error with query', err));
+  return levels;
+};
 
 
 // Defining Song Type //
@@ -65,20 +65,20 @@ const levelType = new GraphQLObjectType({
   name: 'Level',
   description: 'A representation of a playthrough of a song on DJ Max Respect V. Includes: The song chosen, the button style, the difficulty mode, the amount of difficulty stars',
   fields: () => ({
-    id: {type: GraphQLInt},
-    button_mode: {type: GraphQLString},
-    song_id: {type: GraphQLInt},
+    id: { type: GraphQLInt },
+    button_mode: { type: GraphQLString },
+    song_id: { type: GraphQLInt },
     song: {
       type: songType,
       resolve: async (level) => {
         const songs = await getSongs();
-        return songs.find(song => song.id === level.song_id)
+        return songs.find(song => song.id === level.song_id);
       }
     },
-    difficulty: {type: GraphQLString},
-    star_count: {type: GraphQLInt}
+    difficulty: { type: GraphQLString },
+    star_count: { type: GraphQLInt }
   })
-})
+});
 
 
 
@@ -92,20 +92,45 @@ const rootQuery = new GraphQLObjectType({
     songs: {
       type: new GraphQLList(songType),
       description: 'DJ Max Respect Songs',
-      resolve:async () => {
+      resolve: async () => {
         const songs = getSongs();
-        return songs
+        return songs;
       }
     },
     // Return all levels
     levels: {
       type: GraphQLList(levelType),
       description: 'DJ Max Levels',
-      resolve: async ()=>{
-        const levels = getLevels()
-        return levels
+      resolve: async () => {
+        const levels = getLevels();
+        return levels;
       }
     },
+    level: {
+      type: levelType,
+      description: 'One DJ Max Respect V level',
+      args: { difficulty: { type: GraphQLString }, button_mode: {type: GraphQLString} },
+      resolve: async (parent,args) => {
+        let level;
+        const client = new pg.Client(dbConnection);
+        await client.connect()
+          .catch((err) => console.log('Error connecting to DB', err));
+
+        let query;
+        let searchParam;
+
+        if(args.difficulty){
+          query = 'Select * from levels where difficulty like $1';
+        }
+
+   
+        await client.query(query, [args.difficulty])
+        .then((res)=>{level = res.rows})
+        .catch((err)=>{console.log('Oops', err)})
+        return level
+
+      }
+    }
   })
 
 });
